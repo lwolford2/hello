12e1949f1f95a443011761d846afd16ddbb7a4d6
lwolford2 <111825062+lwolford2@users.noreply.github.com>
2022-08-25 15:29:51 +0000

    fixed standard library bug

--- hello.c
+++ hello.c
@@ -2,10 +2,14 @@
  * Basic Hello World Program
  */
 
+#include <stdlib.h>
+#include <stdio.h>
+
+
 int main(int argc, char **argv) {
 
         printf("Hello I'm Logan Wolford!\n");
-        printf("Major is Computer Engineering!\n);
+        printf("Major is Computer Engineering!\n");
 
         return 0;
 }
