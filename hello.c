333e07fa390ca4886dc388db7944962b7efe94b5
lwolford2 <111825062+lwolford2@users.noreply.github.com>
2022-08-25 15:41:30 +0000

    added my minor

--- hello.c
+++ hello.c
@@ -10,6 +10,7 @@ int main(int argc, char **argv) {
 
         printf("Hello I'm Logan Wolford!\n");
         printf("Major is Computer Engineering!\n");
+        printf(Minor is Spanish\n");
 
         return 0;
 }
