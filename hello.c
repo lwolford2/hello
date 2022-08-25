dbf55ae52c07b0624afb97cec33d54143b3139e8
lwolford2 <111825062+lwolford2@users.noreply.github.com>
2022-08-25 15:04:11 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello World Program
+ */
+
+int main(int argc, char **argv) {
+
+        printf("Hello I'm Logan Wolford!\n");
+        printf("Major is Computer Engineering!\n);
+
+        return 0;
+}
