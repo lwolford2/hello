be9db22d2cb22b3fa6d7d894dc10ee609e57b54a
mschmidt15 <111908597+mschmidt15@users.noreply.github.com>
2022-08-29 20:58:19 +0000

    added partner to output

--- hello.c
+++ hello.c
@@ -10,7 +10,11 @@ int main(int argc, char **argv) {
 
         printf("Hello I'm Logan Wolford!\n");
         printf("Major is Computer Engineering!\n");
-        printf(Minor is Spanish\n");
+        printf("Minor is Spanish\n");
+
+        //Matt
+        printf("Matthew Schmidt!\n");
+        printf("Major in Computer Engineering!\n");
 
         return 0;
 }
